d760b078d340b70c79ac525ff992e8537898a62b
jyothishkm <jyothishkm88@gmail.com>
2016-10-10 11:28:08 +0530

    H_number

---
+++ HarmonicNumber.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.programs;
+import com.bridgelabz.util.Utility;
+public class HarmonicNumber{
+	public static void main(String[] args){
+		int num,i;
+		double value = 0;
+		Utility u = new Utility();
+		System.out.println("Please Enter N");
+		num = u.inputInteger();
+		//Giving precondition if number is zero
+		if(num==0){
+			System.out.println("Value of N can not be zero");
+		}
+		//finding the value
+		else{
+			for(i=1;i<=num;i++){
+				value = value + (1.0f/i);
+			}
+			System.out.println("Harmonic value of "+num+" is "+value);
+		}
+
+	}
+}
