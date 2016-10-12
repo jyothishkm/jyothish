0e22420a0c54d4abe64803268907b4669808f371
jyothishkm <jyothishkm88@gmail.com>
2016-10-12 09:24:34 +0530

    Sum equals Zero

---
+++ SumOfThreeInt.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.programs;
+import com.bridgelabz.util.Utility;
+
+public class SumOfThreeInt {
+ public static void main(String[] args) {
+ Utility u = new Utility();
+		System.out.println("Please Enter the no. of Elements");
+		int size = u.inputInteger();
+		int count = 0;
+		int[] array = new int[size];
+		//taking array of numbers from user
+		for(int i=0;i<size;i++){
+			System.out.println("Please enter element at position "+(i+1));
+			array[i] = u.inputInteger();
+		}
+		
+		//to find triples
+		for(int i=0;i<size-2;i++){
+			for(int j=i+1;j<size-1;j++){
+				for(int k=j+1;k<size;k++){
+					if(array[i]+array[j]+array[k] == 0){
+						System.out.println("Triples are :"+array[i]+" "+array[j]+" "+array[k]);
+						count ++;
+					}
+				}
+			}
+		}
+		if(count != 0)
+			System.out.println("Total number of Tripels in given list is: "+count);
+		else
+			System.out.println("Triples are Not Present");
+
+
+ }
+}
